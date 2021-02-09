fb83db03250f1537e92f2d03867bc76252211785
Qamar Islam <qislam@outlook.com>
2021-02-09 13:47:23 -0500

    added basic structure for package command

--- src/commands/package.js
+++ src/commands/package.js
@@ -1,4 +1,5 @@
 const {Command, flags, args} = require('@oclif/command')
+const {cli} = require('cli-ux')
 const debug = require('debug')('qdx')
 const path = require('path')
 const fs = require('fs')
@@ -6,15 +7,36 @@ const YAML = require('yaml')
 const sfdx = require('sfdx-node')
 const csvjson = require('csvjson')
 const xmljs = require('xml-js')
+const execa = require('execa')
 const _ = require('lodash')
-const simpleGit = require('simple-git')
-const git = simpleGit()
 
 class PackageCommand extends Command {
   async run() {
     const {flags, args} = this.parse(PackageCommand)
     debug('args: ' + JSON.stringify(args, null, 4))
     debug('flags: ' + JSON.stringify(flags, null, 4))
+    cli.action.start('Started on package ' + args.packageName)
+
+    if (flags.start || !fs.existsSync(`manifest/${args.packageName.replace('/', '-')}.yml`)) {
+      if (!fs.existsSync('manifest')) {
+        debug('Creating manifest dir')
+        fs.mkdirSync('manifest')
+      }
+      debug('Starting blank package YAML file.')
+      fs.writeFileSync(
+        `manifest/${args.packageName.replace('/', '-')}.yml`,
+        YAML.stringify({Version: flags.version || '50.0'}),
+        {encoding: 'utf-8'}
+      )
+    }
+
+    if (flags.diff) {
+      if (!args.commit1 || !args.commit2) {
+        cli.action.stop('Commit hashes are required with diff flag.')
+      }
+      const diffFiles = await execa('git', ['diff', '--name-only', args.commit1, args.commit2])
+      debug('diffFiles: \n' + JSON.stringify(diffFiles, null, 4))
+    }
   }
 }
 
